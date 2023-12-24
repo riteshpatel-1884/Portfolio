e8baa450805d77660499b01baad96565f4ce9e84
Ritesh Patel <129499361+riteshpatel1884@users.noreply.github.com>
2023-12-24 16:16:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function openMenu() {
     nav.classList.toggle('open');
 }
 AOS.init();
+
+ function sendEmail() {
+        
+        var recipientEmail = 'contact@riteshpatel.in';
+
+        // Create the mailto link
+        var mailtoLink = 'mailto:' + recipientEmail;
+
+        // Open the user's default email client
+        window.location.href = mailtoLink;
+    }
